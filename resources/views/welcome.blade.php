522afb229b243d8591750ff0be3f1a3f52a977f7
mobydickens <steadmegan@gmail.com>
2019-02-19 16:08:06 -0700

    made a new controller file for my routes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,5 @@
 @extends('layout')
 
 @section('content')
-  <h1>My First Website</h1>
-  <ul>
-    @foreach($tasks as $task)
-      <li><?= $task; ?></li>
-    @endforeach
-  </ul>
+  <h1>My {{ $foo }} Website</h1>
 @endsection

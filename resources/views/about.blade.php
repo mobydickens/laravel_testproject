cb0a6d726edffce1a35924f717e6bb1aaffa3c97
mobydickens <steadmegan@gmail.com>
2019-02-19 15:32:23 -0700

    my first laravel project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+
+@section('content')
+  <h1>About Page</h1>
+  <p>Test test test test test</p>
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('title', 'Contact us')
+
+@section('content')
+  <h1>Contact us</h1>
+@endsection

01f9a738c00fe52c9c9606eb83991582052abb0e
mobydickens <steadmegan@gmail.com>
2019-02-22 11:45:57 -0700

    up to episode 12 in laracasts

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Document</title>
+</head>
+<body>
+  <h1>Create a new project</h1>
+
+  <form method='POST' action='/projects'>
+
+    {{ csrf_field() }}
+    
+    <div>
+      <input type="text" name='title' placeholder='project title'>
+    </div>
+    <div>
+      <textarea type="text" name='description' placeholder='project description'></textarea>
+    </div>
+    <div>
+      <button type='submit'>Create</button>
+    </div>
+  </form>
+</body>
+</html> 
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,30 @@
 |
 */
 
-Route::get('/', 'PagesController@home');
+Route::get('/', 'PagesController@home'); 
 Route::get('/about', 'PagesController@about');
 Route::get('/contact', 'PagesController@contact');
+
+
+Route::resource('projects', 'ProjectsController');
+
+// Route::get('/projects', 'ProjectsController@index');
+// // GET /projects -> fetch all of the resource (common convention, call it 'index')
+
+// Route::get('/projects/create', 'ProjectsController@create');
+// // GET /projects/create -> 'create'
+
+// Route::get('/projects/{project}', 'ProjectsController@show');
+// // GET /projects/1 -> 'show'
+
+// Route::post('/projects', 'ProjectsController@store');
+// // POST /projects -> 'store' 
+
+// Route::get('/projects/{project}/edit', 'ProjectsController@edit');
+// // GET /projects/1/edit -> 'edit'
+
+// Route::patch('/projects/{project}', 'ProjectsController@update');
+// // PATCH /projects/1 -> 'update'
+
+// Route::delete('/projects/{project}', 'ProjectsController@destroy');
+// // DELETE /projects/1 -> 'destroy'
